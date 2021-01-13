2f9b28a0343f63cc7fc411f97072e11e2fbfbfd8
Junie-CVP <juniesenthilkumar@cvpcorp.com>
2021-01-13 16:47:28 -0500

    removed unused import

--- src/hourly-apportioned-emissions/hourly-apportioned-emissions.service.ts
+++ src/hourly-apportioned-emissions/hourly-apportioned-emissions.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Request } from 'express';
 
