f56193c244c2b182b4b7e31de100ab1e42b5c9c6
Junie-CVP <juniesenthilkumar@cvpcorp.com>
2021-01-25 08:36:38 -0500

    formatted unit tests for params DTO

--- src/dto/hourly-apportioned-emissions.params.dto.spec.ts
+++ src/dto/hourly-apportioned-emissions.params.dto.spec.ts
@@ -1,4 +1,4 @@
-import { Validate, validate } from 'class-validator';
+import { IsDefined, Validate, validate } from 'class-validator';
 import * as typeorm from 'typeorm';
 import { createSandbox, SinonSandbox, createStubInstance } from 'sinon';
 
@@ -14,79 +14,95 @@ import { StateValidation } from '../pipes/state-validation.pipe';
 
 describe('-- Hourly Apportioned Emissions Params DTO --', () => {
   describe('* getHourlyEmissions', () => {
-    it('tests the validations for the params DTO', async () => {
-      class MyClass {
-        constructor(
-          beginDate: string,
-          endDate: string,
-          orisCode: string,
-          control: string,
-          unitType: string,
-          unitFuel: string,
-          state: string,
-        ) {
-          this.beginDate = beginDate;
-          this.endDate = endDate;
-          this.orisCode = orisCode;
-          this.control = control;
-          this.unitType = unitType;
-          this.unitFuel = unitFuel;
-          this.state = state;
-        }
-        @IsInDateRange([new Date('1995-01-01'), new Date()])
-        @IsValidDate()
-        @IsIsoFormat()
-        beginDate: string;
-
-        @IsDateGreaterThanEqualTo('beginDate')
-        @IsInDateRange([new Date('1995-01-01'), new Date()])
-        @IsValidDate()
-        @IsIsoFormat()
-        endDate: string;
-
-        @IsOrisCode()
-        orisCode: string;
-
-        @IsControlTechnology()
-        control: string;
-
-        @IsUnitType()
-        unitType: string;
-
-        @IsUnitFuelType()
-        unitFuel: string;
-
-        @Validate(StateValidation)
-        state: string;
+    class MyClass {
+      constructor(
+        beginDate: string,
+        endDate: string,
+        orisCode: string,
+        control: string,
+        unitType: string,
+        unitFuel: string,
+        state: string,
+      ) {
+        this.beginDate = beginDate;
+        this.endDate = endDate;
+        this.orisCode = orisCode;
+        this.control = control;
+        this.unitType = unitType;
+        this.unitFuel = unitFuel;
+        this.state = state;
       }
+      @IsInDateRange([new Date('1995-01-01'), new Date()])
+      @IsValidDate()
+      @IsIsoFormat()
+      @IsDefined()
+      beginDate: string;
 
-      /**
-       * This class is used to mock EntityManager and ConnectionManager
-       */
-      class Mock {
-        sandbox: SinonSandbox;
-        constructor(method: string | any, fakeData: any, args?: any) {
-          this.sandbox = createSandbox();
-          if (args) {
-            this.sandbox
-              .stub(typeorm, method)
-              .withArgs(args)
-              .returns(fakeData);
-          } else {
-            this.sandbox.stub(typeorm, method).returns(fakeData);
-          }
-        }
-        close() {
-          this.sandbox.restore();
+      @IsDateGreaterThanEqualTo('beginDate')
+      @IsInDateRange([new Date('1995-01-01'), new Date()])
+      @IsValidDate()
+      @IsIsoFormat()
+      @IsDefined()
+      endDate: string;
+
+      @IsOrisCode()
+      orisCode: string;
+
+      @IsControlTechnology()
+      control: string;
+
+      @IsUnitType()
+      unitType: string;
+
+      @IsUnitFuelType()
+      unitFuel: string;
+
+      @Validate(StateValidation)
+      state: string;
+    }
+
+    /**
+     * This class is used to mock EntityManager and ConnectionManager
+     */
+    class Mock {
+      sandbox: SinonSandbox;
+      constructor(method: string | any, fakeData: any, args?: any) {
+        this.sandbox = createSandbox();
+        if (args) {
+          this.sandbox
+            .stub(typeorm, method)
+            .withArgs(args)
+            .returns(fakeData);
+        } else {
+          this.sandbox.stub(typeorm, method).returns(fakeData);
         }
       }
+      close() {
+        this.sandbox.restore();
+      }
+    }
+    let mock: Mock;
+    const fakeManager = createStubInstance(typeorm.EntityManager);
+    fakeManager.findOne.resolves(['value']);
+    mock = new Mock('getManager', fakeManager);
 
-      let mock: Mock;
-      const fakeManager = createStubInstance(typeorm.EntityManager);
-      fakeManager.findOne.resolves(['value']);
-      mock = new Mock('getManager', fakeManager);
+    it('should pass all validation pipes', async () => {
+      const results = await validate(
+        new MyClass(
+          '2019-01-01',
+          '2019-01-01',
+          '612',
+          'control',
+          'unitType',
+          'unitFuel',
+          'state',
+        ),
+      );
+      expect(results.length).toBe(0);
+    });
 
-      let results = await validate(
+    it('should fail one of validation pipes (beginDate)', async () => {
+      const results = await validate(
         new MyClass(
           '1990-01-01',
           '2020-01-01',
@@ -97,31 +113,24 @@ describe('-- Hourly Apportioned Emissions Params DTO --', () => {
           'state',
         ),
       );
-      results = await validate(
-        new MyClass(
-          '2020-02-31',
-          '2020-03-01',
-          '612',
-          'control',
-          'unitType',
-          'unitFuel',
-          'state',
-        ),
-      );
+      expect(results.length).toBe(1);
+    });
+
+    it('should fail all of the validation pipes', async () => {
       fakeManager.findOne.resolves(null);
-      results = await validate(
+      const results = await validate(
         new MyClass(
           null,
           'error',
-          '612',
+          '0',
           'control',
           'unitType',
           'unitFuel',
           'state',
         ),
       );
-      expect(results.length).toBeGreaterThan(0);
-      mock.close;
+      expect(results.length).toBe(7);
     });
+    mock.close;
   });
 });
