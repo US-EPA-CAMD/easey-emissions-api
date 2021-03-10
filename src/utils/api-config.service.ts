e69e90bd85c3322ece2ee143bcfedb1fb3e1b735
Junie-CVP <juniesenthilkumar@cvpcorp.com>
2021-03-10 16:56:48 -0500

    updated ApiConfigService

--- src/utils/api-config.service.ts
+++ src/utils/api-config.service.ts
@@ -6,10 +6,10 @@ export class ApiConfigService {
     const host = process.env.EASEY_EMISSIONS_API_HOST || 'localhost';
 
     if (host === 'localhost') {
-      const port = process.env.EASEY_EMISSIONS_MGMT_API_PORT || 8080;
+      const port = process.env.EASEY_EMISSIONS_API_PORT || 8080;
       return `localhost:${port}`;
     }
-    return process.env.EASEY_API_HOST;
+    return host;
   }
 
   public static getMdm(): string {
