ccbba7939f42c6f3f2febb8c22026276e95247ec
Junie-CVP <juniesenthilkumar@cvpcorp.com>
2021-03-10 16:43:35 -0500

    updated env host name in ApiConfigService

--- src/utils/api-config.service.ts
+++ src/utils/api-config.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ApiConfigService {
   public static getHost(): string {
-    const host = process.env.EASEY_API_HOST || 'localhost';
+    const host = process.env.EASEY_EMISSIONS_API_HOST || 'localhost';
 
     if (host === 'localhost') {
       const port = process.env.EASEY_EMISSIONS_MGMT_API_PORT || 8080;
