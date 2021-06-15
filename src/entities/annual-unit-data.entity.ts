818344ad7592991275a9cec861276b4eab3228d6
Sejal Dhawan <sejaldhawan@cvpcorp.com>
2021-06-15 15:45:26 -0400

    added annual emissions endpoint

---
+++ src/entities/annual-unit-data.entity.ts
@@ -0,0 +1,90 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from 'typeorm';
+
+import { UnitFact } from './unit-fact.entity';
+
+@Entity({ name: 'camddmw.annual_unit_data' })
+export class AnnualUnitData extends BaseEntity {
+  @PrimaryColumn({
+    name: 'unit_id',
+  })
+  unitId: number;
+
+  @PrimaryColumn({
+    name: 'op_year',
+  })
+  opYear: number;
+
+  @Column({
+    name: 'count_op_time',
+  })
+  countOpTime: number;
+
+  @Column({
+    name: 'sum_op_time',
+  })
+  sumOpTime: number;
+
+  @Column()
+  gload: number;
+
+  @Column()
+  sload: number;
+
+  @Column({
+    name: 'heat_input',
+  })
+  heatInput: number;
+
+  @Column({
+    name: 'nox_mass',
+  })
+  noxMass: number;
+
+  @Column({
+    name: 'nox_rate',
+  })
+  noxRate: number;
+
+  @Column({
+    name: 'so2_mass',
+  })
+  so2Mass: number;
+
+  @Column({
+    name: 'so2_rate',
+  })
+  so2Rate: number;
+
+  @Column({
+    name: 'co2_mass',
+  })
+  co2Mass: number;
+
+  @Column({
+    name: 'co2_rate',
+  })
+  co2Rate: number;
+
+  @ManyToOne(
+    () => UnitFact,
+    uf => uf.annualUnitData,
+  )
+  @JoinColumn([
+    {
+      name: 'unit_id',
+      referencedColumnName: 'id',
+    },
+    {
+      name: 'op_year',
+      referencedColumnName: 'opYear',
+    },
+  ])
+  unitFact: UnitFact;
+}
