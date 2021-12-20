a5333dab3a675d75788bcec31d8de0a9554313ca
Kyle-Herceg <kyleherceg@cvpcorp.com>
2021-12-20 15:29:15 -0500

    PR changes to DTO

--- src/dto/emissions-submission-progress.dto.ts
+++ src/dto/emissions-submission-progress.dto.ts
@@ -1,13 +1,13 @@
 export class EmissionsSubmissionsProgressDTO {
     beginDate: Date;
     endDate: Date;
-    calendarYear: number;
-    quarter: number;
-    submittedPercentage: number;
-    submittedCount: number;
-    remainingCount: number;
-    totalExpectedCount: number;
-    gdmUsedPercentage: number;
-    gdmUsedCount: number;
-    gdmRemainingCount: number;
+    calendarYear?: number;
+    quarter?: number;
+    submittedPercentage?: number;
+    submittedCount?: number;
+    remainingCount?: number;
+    totalExpectedCount?: number;
+    gdmUsedPercentage?: number;
+    gdmUsedCount?: number;
+    gdmRemainingCount?: number;
 }
