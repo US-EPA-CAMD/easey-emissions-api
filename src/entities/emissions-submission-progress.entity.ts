f330b8f29d6670496d8029e7a47c9c53116f48d9
Jason Whitehead <jwhitehead@austin.rr.com>
2022-05-27 12:50:34 -0500

    Fixes bug #3482 display correct quarter on emissions submission progress bar

--- src/entities/emissions-submission-progress.entity.ts
+++ src/entities/emissions-submission-progress.entity.ts
@@ -1,4 +1,5 @@
 import { ViewEntity, ViewColumn, BaseEntity } from 'typeorm';
+import { NumericColumnTransformer } from '@us-epa-camd/easey-common/transforms';
 
 @ViewEntity({ name: 'camddmw.vw_emissions_submissions_progress' })
 export class EmissionSubmissionsProgress extends BaseEntity {
@@ -14,46 +15,55 @@ export class EmissionSubmissionsProgress extends BaseEntity {
 
   @ViewColumn({
     name: 'calendar_year',
+    transformer: new NumericColumnTransformer(),    
   })
   calendarYear: number;
 
   @ViewColumn({
     name: 'quarter',
+    transformer: new NumericColumnTransformer(),    
   })
   quarter: number;
 
   @ViewColumn({
     name: 'submitted_percentage',
+    transformer: new NumericColumnTransformer(),    
   })
   submittedPercentage: number;
 
   @ViewColumn({
     name: 'submitted_count',
+    transformer: new NumericColumnTransformer(),    
   })
   submittedCount: number;
 
   @ViewColumn({
     name: 'remaining_count',
+    transformer: new NumericColumnTransformer(),    
   })
   remainingCount: number;
 
   @ViewColumn({
     name: 'total_expected_count',
+    transformer: new NumericColumnTransformer(),    
   })
   totalExpectedCount: number;
 
   @ViewColumn({
     name: 'gdm_used_percentage',
+    transformer: new NumericColumnTransformer(),    
   })
   gdmUsedPercentage: number;
 
   @ViewColumn({
     name: 'gdm_used_count',
+    transformer: new NumericColumnTransformer(),    
   })
   gdmUsedCount: number;
 
   @ViewColumn({
     name: 'gdm_remaining_count',
+    transformer: new NumericColumnTransformer(),    
   })
   gdmRemainingCount: number;
 }
