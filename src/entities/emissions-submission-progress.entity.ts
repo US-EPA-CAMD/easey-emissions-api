d042d3e2c0a46502311efd7245cced856df8043a
Kyle-Herceg <kyleherceg@cvpcorp.com>
2022-07-18 10:36:12 -0400

    Converted schema of emissions submissions to camdecmpsmd

--- src/entities/emissions-submission-progress.entity.ts
+++ src/entities/emissions-submission-progress.entity.ts
@@ -1,7 +1,7 @@
 import { ViewEntity, ViewColumn, BaseEntity } from 'typeorm';
 import { NumericColumnTransformer } from '@us-epa-camd/easey-common/transforms';
 
-@ViewEntity({ name: 'camddmw.vw_emissions_submissions_progress' })
+@ViewEntity({ name: 'camdecmpsmd.vw_emissions_submissions_progress' })
 export class EmissionSubmissionsProgress extends BaseEntity {
   @ViewColumn({
     name: 'begin_date',
@@ -15,55 +15,55 @@ export class EmissionSubmissionsProgress extends BaseEntity {
 
   @ViewColumn({
     name: 'calendar_year',
-    transformer: new NumericColumnTransformer(),    
+    transformer: new NumericColumnTransformer(),
   })
   calendarYear: number;
 
   @ViewColumn({
     name: 'quarter',
-    transformer: new NumericColumnTransformer(),    
+    transformer: new NumericColumnTransformer(),
   })
   quarter: number;
 
   @ViewColumn({
     name: 'submitted_percentage',
-    transformer: new NumericColumnTransformer(),    
+    transformer: new NumericColumnTransformer(),
   })
   submittedPercentage: number;
 
   @ViewColumn({
     name: 'submitted_count',
-    transformer: new NumericColumnTransformer(),    
+    transformer: new NumericColumnTransformer(),
   })
   submittedCount: number;
 
   @ViewColumn({
     name: 'remaining_count',
-    transformer: new NumericColumnTransformer(),    
+    transformer: new NumericColumnTransformer(),
   })
   remainingCount: number;
 
   @ViewColumn({
     name: 'total_expected_count',
-    transformer: new NumericColumnTransformer(),    
+    transformer: new NumericColumnTransformer(),
   })
   totalExpectedCount: number;
 
   @ViewColumn({
     name: 'gdm_used_percentage',
-    transformer: new NumericColumnTransformer(),    
+    transformer: new NumericColumnTransformer(),
   })
   gdmUsedPercentage: number;
 
   @ViewColumn({
     name: 'gdm_used_count',
-    transformer: new NumericColumnTransformer(),    
+    transformer: new NumericColumnTransformer(),
   })
   gdmUsedCount: number;
 
   @ViewColumn({
     name: 'gdm_remaining_count',
-    transformer: new NumericColumnTransformer(),    
+    transformer: new NumericColumnTransformer(),
   })
   gdmRemainingCount: number;
 }
