84a153cc0768796bbfcf8894565529aac7d979f7
ethang <hello@ethang.email>
2022-08-09 14:36:46 -0500

    feature(3943): API: Define Long Term Fuel Flow DTOs
    * Naming fix

--- src/dto/long-term-fuel-flow.dto.ts
+++ src/dto/long-term-fuel-flow.dto.ts
@@ -1,5 +1,24 @@
-export class LongTermFuelFlowBaseDTO {}
-export class LongTermFuelFlowRecordDTO extends LongTermFuelFlowBaseDTO {}
+export class LongTermFuelFlowBaseDTO {
+  stackPipeId?: string;
+  unitId?: string;
+  monitoringSystemId?: string;
+  fuelFlowPeriodCode?: string;
+  longTermFuelFlowValue?: number;
+  longTermFuelFlowUomCode?: string;
+  grossCalorificValue?: number;
+  gcvUnitsOfMeasureCode?: string;
+  totalHeatInput?: number;
+}
+
+export class LongTermFuelFlowRecordDTO extends LongTermFuelFlowBaseDTO {
+  id: string;
+  reportingPeriodId?: number;
+  monitoringLocationId?: string;
+  calcTotalHeatInput?: number;
+  userId?: string;
+  addDate?: Date;
+  updateDate?: Date;
+}
 
 export class LongTermFuelFlowImportDTO extends LongTermFuelFlowBaseDTO {}
 
