270f892f2dc626ac94284e19dbc6745553bec851
Jason Whitehead <jwhitehead@austin.rr.com>
2022-10-01 16:43:32 -0500

    configuration & workflow refactor plus update to latest common

--- src/main.ts
+++ src/main.ts
@@ -10,19 +10,23 @@ import { AppModule } from './app.module';
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await applyMiddleware(AppModule, app);
+  await applyMiddleware(AppModule, app, true);
   await applySwagger(app);
 
   const configService = app.get(ConfigService);
   const appPath = configService.get<string>('app.path');
   const appPort = configService.get<number>('app.port');
+  const enableDebug = configService.get<boolean>('app.enableDebug');
 
   const server = await app.listen(appPort);
   server.setTimeout(1800000);
 
-  console.log(
-    `Application is running on: ${await app.getUrl()}/${appPath}/swagger`,
-  );
+  if (enableDebug) {
+    console.log('config: ', configService.get('app'));
+    console.log(
+      `Application is running on: ${await app.getUrl()}/${appPath}/swagger`,
+    );
+  }
 }
 
 bootstrap();
