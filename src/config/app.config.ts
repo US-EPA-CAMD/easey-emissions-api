01f94c84ba13736dd7d71742bf093bc0a85a8856
Jason Whitehead <jwhitehead@austin.rr.com>
2022-10-03 13:56:38 -0500

    restructuring env var configuraiton

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -7,19 +7,21 @@ import {
 
 require('dotenv').config();
 
-const path = getConfigValue('EASEY_EMISSIONS_API_PATH', 'emissions-mgmt');
 const host = getConfigValue('EASEY_EMISSIONS_API_HOST', 'localhost');
 const port = getConfigValueNumber('EASEY_EMISSIONS_API_PORT', 8040);
-
-export const PAGINATION_MAX_PER_PAGE = getConfigValueNumber(
-  'EASEY_EMISSIONS_API_PAGINATION_MAX_PER_PAGE', 500,
-);
+const path = getConfigValue('EASEY_EMISSIONS_API_PATH', 'emissions-mgmt');
 
 let uri = `https://${host}/${path}`;
 
 if (host === 'localhost') {
   uri = `http://localhost:${port}/${path}`;
 }
+
+const apiHost = getConfigValue('EASEY_API_GATEWAY_HOST', 'api.epa.gov/easey/dev');
+
+export const PAGINATION_MAX_PER_PAGE = getConfigValueNumber(
+  'EASEY_EMISSIONS_API_PAGINATION_MAX_PER_PAGE', 500,
+);
 
 export default registerAs('app', () => ({
   name: 'emissions-api',
@@ -31,17 +33,23 @@ export default registerAs('app', () => ({
     'EASEY_EMISSIONS_API_DESCRIPTION',
     'Emissions management API endpoints for apportioned emissions data (e.g. hourly, daily, monthly, annual, and ozone season)',
   ),
-  apiHost: getConfigValue(
-    'EASEY_API_GATEWAY_HOST', 'api.epa.gov/easey/dev',
-  ),
   env: getConfigValue(
     'EASEY_EMISSIONS_API_ENV', 'local-dev',
   ),
-  enableCors: getConfigValueBoolean(
-    'EASEY_EMISSIONS_API_ENABLE_CORS', true,
+  apiKey: getConfigValue(
+    'EASEY_EMISSIONS_API_KEY',
   ),
   enableApiKey: getConfigValueBoolean(
     'EASEY_EMISSIONS_API_ENABLE_API_KEY',
+  ),
+  secretToken: getConfigValue(
+    'EASEY_EMISSIONS_API_SECRET_TOKEN',
+  ),
+  enableSecretToken: getConfigValueBoolean(
+    'EASEY_EMISSIONS_API_ENABLE_SECRET_TOKEN',
+  ),
+  enableCors: getConfigValueBoolean(
+    'EASEY_EMISSIONS_API_ENABLE_CORS', true,
   ),
   enableGlobalValidationPipes: getConfigValueBoolean(
     'EASEY_EMISSIONS_API_ENABLE_GLOBAL_VALIDATION_PIPE', true,
@@ -52,18 +60,13 @@ export default registerAs('app', () => ({
   published: getConfigValue(
     'EASEY_EMISSIONS_API_PUBLISHED', 'local',
   ),
-  perPageLimit: PAGINATION_MAX_PER_PAGE,
   submissionDays: getConfigValueNumber(
     'EASEY_EMISSIONS_API_SUBMISSION_DAYS', 38,
-  ),
-  secretToken: getConfigValue(
-    'EASEY_EMISSIONS_API_SECRET_TOKEN',
-  ),
-  enableSecretToken: getConfigValueBoolean(
-    'EASEY_EMISSIONS_API_ENABLE_SECRET_TOKEN',
   ),
   // ENABLES DEBUG CONSOLE LOGS
   enableDebug: getConfigValueBoolean(
     'EASEY_EMISSIONS_API_ENABLE_DEBUG',
   ),
+  perPageLimit: PAGINATION_MAX_PER_PAGE,
+  apiHost: apiHost,
 }));
