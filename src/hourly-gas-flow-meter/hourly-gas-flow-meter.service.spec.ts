740336fcdce1ef26b18ed82da62b1ece2301fd77
Ethan Glover <hello@ethang.email>
2022-10-06 11:05:40 -0500

    feature(3868): API: Export NSPS4T Summary, Compliance Period, 4th Quarter Data
    * Import fix

--- src/hourly-gas-flow-meter/hourly-gas-flow-meter.service.spec.ts
+++ src/hourly-gas-flow-meter/hourly-gas-flow-meter.service.spec.ts
@@ -1,7 +1,7 @@
 import { HourlyGasFlowMeterRepository } from './hourly-gas-flow-meter.repository';
 import { Test } from '@nestjs/testing';
 import { HourlyGasFlowMeterMap } from '../maps/hourly-gas-flow-meter.map';
-import { HourlyGasFlowMeterService } from '../hourly-gas-flow-meter/hourly-gas-flow-meter.service';
+import { HourlyGasFlowMeterService } from './hourly-gas-flow-meter.service';
 import { mockHourlyGasFlowMeterRepository } from '../../test/mocks/mock-hourly-gas-flow-meter-repository';
 import { genHourlyGasFlowMeter } from '../../test/object-generators/hourly-gas-flow-meter';
 import { HrlyGasFlowMeter } from '../entities/workspace/hrly-gas-flow-meter.entity';
