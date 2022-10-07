88ebf7c961b31ca3f628354feed23344e354530a
Ethan Glover <hello@ethang.email>
2022-10-07 11:12:33 -0500

    feature(4380): PR

--- src/hourly-operating/hourly-operating.controller.ts
+++ src/hourly-operating/hourly-operating.controller.ts
@@ -5,18 +5,30 @@ import {
   Query,
   UseInterceptors,
 } from '@nestjs/common';
-import { ApiOkResponse, ApiSecurity, ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiQuery, ApiSecurity, ApiTags } from '@nestjs/swagger';
 import { HourlyOperatingService } from './hourly-operating.service';
 import { HourlyOperatingParamsDto } from '../dto/hourly-operating.params.dto';
 import { HourlyOperatingDTO } from '../dto/hourly-operating.dto';
 
-@ApiTags('Emissions')
+@ApiTags('Hourly Operating')
 @Controller('emissions')
 @ApiSecurity('APIKey')
 export class HourlyOperatingController {
   constructor(private readonly service: HourlyOperatingService) {}
 
   @Get('hourly-operating-data')
+  @ApiQuery({
+    style: 'pipeDelimited',
+    name: 'orisCode',
+    required: false,
+    explode: false,
+  })
+  @ApiQuery({
+    style: 'pipeDelimited',
+    name: 'locationName',
+    required: false,
+    explode: false,
+  })
   @ApiOkResponse({
     description:
       'Exports hourly operating data for specified ORIS codes and reporting period.',
