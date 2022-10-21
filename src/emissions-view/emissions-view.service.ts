bc4fd650e1a47d8cb4931659041c2b304dcceb6f
sdhawan16 <sejaldhawan@cvpcorp.com>
2022-10-21 14:48:47 -0400

    added columns to header and modified endpoint to accept an accept header

--- src/emissions-view/emissions-view.service.ts
+++ src/emissions-view/emissions-view.service.ts
@@ -1,16 +1,14 @@
 import { getManager } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
+
 import { EmissionsViewDTO } from '../dto/emissions-view.dto';
-import { EmissionsViewResultsDTO } from '../dto/emissions-view-results.dto';
 import { EmissionsViewParamsDTO } from '../dto/emissions-view.params.dto';
 import { EmissionsViewRepository } from './emissions-view.repository';
 
-
 @Injectable()
 export class EmissionsViewService {
-  constructor(
-    private readonly repository: EmissionsViewRepository,
-  ) {}
+  constructor(private readonly repository: EmissionsViewRepository) {}
 
   async getAvailableViews(): Promise<EmissionsViewDTO[]> {
     const results = await this.repository.find({ templateCode: 'EMVIEW' });
@@ -19,32 +17,39 @@ export class EmissionsViewService {
       return {
         code: e.code,
         name: e.displayName,
-      }
+      };
     });
   }
 
-  async getView(viewCode: string, params: EmissionsViewParamsDTO): Promise<EmissionsViewResultsDTO> {
+  async getView(
+    viewCode: string,
+    req: Request,
+    params: EmissionsViewParamsDTO,
+  ) {
     const mgr = getManager();
     const schema = 'camdecmpswks';
 
-    const rptPeriod = await mgr.query(`
+    const rptPeriod = await mgr.query(
+      `
       SELECT rpt_period_id AS "id"
       FROM camdecmpsmd.reporting_period
       WHERE calendar_year = $1 AND quarter = $2;`,
-      [params.year, params.quarter]
+      [params.year, params.quarter],
     );
 
-    const monLocs = await mgr.query(`
+    const monLocs = await mgr.query(
+      `
       SELECT ml.mon_loc_id AS "id"
       FROM ${schema}.monitor_location ml
       JOIN ${schema}.monitor_plan_location mpl USING(mon_loc_id)
       LEFT JOIN camd.unit u USING(unit_id)
       LEFT JOIN ${schema}.stack_pipe sp USING(stack_pipe_id)
       WHERE mpl.mon_plan_id = $1 AND (u.unitid = ANY($2) OR sp.stack_name = ANY($3));`,
-      [params.monitorPlanId, params.unitIds, params.stackPipeIds]
+      [params.monitorPlanId, params.unitIds, params.stackPipeIds],
     );
 
-    const columns = await mgr.query(`
+    const columns = await mgr.query(
+      `
       SELECT
         ds.display_name AS "viewName",
         ds.no_results_msg AS "noResultsMsg",
@@ -57,29 +62,28 @@ export class EmissionsViewService {
       WHERE ds.template_cd = $1
       AND ds.dataset_cd = $2
       ORDER BY col.column_order`,
-      ['EMVIEW', viewCode.toUpperCase()]
+      ['EMVIEW', viewCode.toUpperCase()],
     );
 
     let columnList = columns.map(i => `${i.columnName} AS "${i.columnAlias}"`);
 
-    const viewData = await mgr.query(`
+    const viewData = await mgr.query(
+      `
       SELECT ${columnList.join(',')}
       FROM ${schema}.emission_view_${viewCode.toLowerCase()}
       WHERE rpt_period_id = $1 AND mon_loc_id = ANY($2)
       LIMIT 2;`,
-      [rptPeriod[0].id, monLocs.map(i => i.id)]
+      [rptPeriod[0].id, monLocs.map(i => i.id)],
     );
 
     columnList = columns.map(i =>
-      JSON.parse(`{ "name": "${i.columnAlias}", "displayName": "${i.columnLabel}" }`)
+      JSON.parse(
+        `{ "label": "${i.columnLabel}", "value": "${i.columnAlias}" }`,
+      ),
     );
 
-    return {
-      code: viewCode.toUpperCase(),
-      name: columns[0].viewName,
-      noResultsMessage: columns[0].noResultsMsg,
-      columns: columnList,
-      results: viewData,
-    }
+    req.res.setHeader('X-Field-Mappings', JSON.stringify(columnList));
+
+    return viewData;
   }
 }
