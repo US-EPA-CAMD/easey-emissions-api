64fd92ec103039c9fea0bb2d976d3dc8fca7411d
Ethan Glover <hello@ethang.email>
2022-11-02 11:25:11 -0500

    feature(4381): From 4380 PR notes.

--- src/derived-hourly-value/derived-hourly-value.controller.ts
+++ src/derived-hourly-value/derived-hourly-value.controller.ts
@@ -7,17 +7,29 @@ import {
   Query,
   UseInterceptors,
 } from '@nestjs/common';
-import { ApiOkResponse, ApiSecurity, ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiQuery, ApiSecurity, ApiTags } from '@nestjs/swagger';
 import { DerivedHourlyValueService } from './derived-hourly-value.service';
 import { DerivedHourlyValueParamsDto } from '../dto/derived-hourly-value-params.dto';
 
-@ApiTags('Emissions')
+@ApiTags('Derived Hourly Value')
 @Controller('emissions')
 @ApiSecurity('APIKey')
 export class DerivedHourlyValueController {
   constructor(private readonly service: DerivedHourlyValueService) {}
 
   @Get('derived-hourly-value')
+  @ApiQuery({
+    style: 'pipeDelimited',
+    name: 'orisCode',
+    required: false,
+    explode: false,
+  })
+  @ApiQuery({
+    style: 'pipeDelimited',
+    name: 'locationName',
+    required: false,
+    explode: false,
+  })
   @ApiOkResponse({
     description:
       'Exports derived hourly values for specified ORIS codes and reporting period.',
