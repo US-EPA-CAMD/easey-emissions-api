7c086bb52cd75c4af52f8060ee2535c6a4fbe654
Jason Whitehead <jwhitehead@austin.rr.com>
2023-06-24 22:12:05 -0500

    support to export reported values only

--- src/emissions/emissions.controller.ts
+++ src/emissions/emissions.controller.ts
@@ -9,16 +9,17 @@ import {
 import {
   ApiExtraModels,
   ApiOkResponse,
+  ApiOperation,
   ApiSecurity,
   ApiTags,
+  refs,
 } from '@nestjs/swagger';
 
 import { EmissionsSubmissionsProgressDTO } from '../dto/emissions-submissions-progress.dto';
 import { EmissionsSubmissionsParamsDTO } from '../dto/emissions-submissions.params.dto';
 import { EmissionsService } from './emissions.service';
-import { EmissionsDTO } from '../dto/emissions.dto';
+import { EmissionsDTO, EmissionsImportDTO } from '../dto/emissions.dto';
 import { EmissionsParamsDTO } from '../dto/emissions.params.dto';
-import { ReviewSubmitService } from '../emissions-workspace/ReviewSubmit.service';
 
 @Controller()
 @ApiTags('Emissions')
@@ -27,12 +28,62 @@ export class EmissionsController {
   constructor(private readonly service: EmissionsService) {}
 
   @Get('export')
+  @ApiOperation({
+    summary: 'Exports emissions data for the specified Monitor Plan & Reporting Period'
+  })
   @ApiOkResponse({
-    description:
-      'Exports emissions data for the specified Monitor Plan & Reporting Period',
+    description: 'Successfull export of emissions data',
+    content: {
+      'application/json': {
+        schema: {
+          oneOf: refs(
+            EmissionsDTO,
+            EmissionsImportDTO,
+          )
+        },
+        examples: {
+          fullExport: {
+            summary: 'Full Export',
+            description: 'Full export of all data including database primary keys, calculated values, & audit properties',
+            value: "Reference EmissionsDTO",
+          },
+          reportedValuesExport: {
+            summary: 'Reported Values Export',
+            description: 'Export of reported values only matching import schema',
+            value: "Reference EmissionsImportDTO",
+          }
+        }
+      }
+    }
   })
   @UseInterceptors(ClassSerializerInterceptor)
-  export(@Query() params: EmissionsParamsDTO): Promise<EmissionsDTO> {
+  export(@Query() params: EmissionsParamsDTO): Promise<EmissionsDTO | EmissionsImportDTO> {
+    return this.service.export(params);
+  }
+
+  @Get('export/all-values')
+  @ApiOperation({
+    summary: 'Exports emissions data for the specified Monitor Plan & Reporting Period'
+  })
+  @ApiOkResponse({
+    type: EmissionsDTO,
+    description: 'Successfull export of all data including database primary keys, calculated values, & audit properties',
+  })
+  @UseInterceptors(ClassSerializerInterceptor)
+  exportAllValues(@Query() params: EmissionsParamsDTO): Promise<EmissionsDTO> {
+    return this.service.export(params);
+  }
+
+  @Get('export/reported-values')
+  @ApiOperation({
+    summary: 'Exports emissions data for the specified Monitor Plan & Reporting Period'
+  })
+  @ApiOkResponse({
+    type: EmissionsImportDTO,
+    description: 'Successfull export of reported values only matching import schema',
+  })
+  @UseInterceptors(ClassSerializerInterceptor)
+  exportReportValues(@Query() params: EmissionsParamsDTO): Promise<EmissionsImportDTO> {
     return this.service.export(params);
   }
 
