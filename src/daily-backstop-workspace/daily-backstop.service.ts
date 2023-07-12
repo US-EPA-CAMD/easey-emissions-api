3c4495219450874f64ed258922aca62dd5137532
Fahim Anwari <fahimanwari@cvpcorp.com>
2023-07-12 04:25:49 -0400

    Readded unit_id for daily backstop data

--- src/daily-backstop-workspace/daily-backstop.service.ts
+++ src/daily-backstop-workspace/daily-backstop.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { ImportIdentifiers } from "../emissions-workspace/emissions.service";
 import { BulkLoadService } from "@us-epa-camd/easey-common/bulk-load";
 import { EmissionsImportDTO } from "../dto/emissions.dto";
+import { MonitorLocation } from "../entities/workspace/monitor-location.entity";
 
 export type DailyBackstopCreate = & {
     reportingPeriodId: number;
@@ -33,6 +34,7 @@ export class DailyBackstopWorkspaceService {
         const bulkLoadStream = await this.bulkLoadService.startBulkLoader(
           'camdecmpswks.daily_backstop',
           [
+            'unit_id',
             'op_date',
             'daily_noxm',
             'daily_hit',
@@ -48,9 +50,9 @@ export class DailyBackstopWorkspaceService {
         );
     
         for (const dailyBackstopDatum of emissionsImport.dailyBackstopData) {
-          const monitoringLocationId = monitoringLocations.filter(location => 
+          const monitoringLocation: MonitorLocation = monitoringLocations.filter(location => 
               location.unit?.name === dailyBackstopDatum.unitId 
-          )[0].id;
+          )[0];
         
           const {
             date,
@@ -62,13 +64,14 @@ export class DailyBackstopWorkspaceService {
           } = dailyBackstopDatum;
     
           bulkLoadStream.writeObject({
+            unitId: monitoringLocation?.unit?.id ?? null,
             date,
             dailyNOxEmissions,
             dailyHeatInput,
             dailyAverageNOxRate,
             dailyNOxExceedence,
             cumulativeOSNOxExceedence,
-            monitoringLocationId,
+            monitoringLocationId: monitoringLocation?.id ?? null,
             reportingPeriodId,
             userId: identifiers?.userId,
             addDate: currentTime,
