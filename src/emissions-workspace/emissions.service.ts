fa42c5ee8c1630ab4ec5997670b510475f181995
subrataroy1 <subrataroy@cvpcorp.com>
2023-09-18 10:48:14 -0400

    fix: update submissionAvailiblityCode

--- src/emissions-workspace/emissions.service.ts
+++ src/emissions-workspace/emissions.service.ts
@@ -291,6 +291,7 @@ export class EmissionsWorkspaceService {
           evalStatusCd: 'EVAL',
           needsEvalFlag: 'Y',
           updatedStatusFlg: 'Y',
+          submissionAvailabilityCd: 'REQUIRE',
           lastUpdated: new Date(),
         }),
       );
