59a02263cf8fdca85bee03c03ded24dabe8770aa
ergjustin <justin.joseph.erg.com>
2024-11-04 14:37:54 -0600

    feat:added ecmps offline middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpModule } from '@nestjs/axios';
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { RouterModule } from 'nest-router';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -10,6 +10,7 @@ import { CorsOptionsModule } from '@us-epa-camd/easey-common/cors-options';
 import { CheckCatalogModule } from '@us-epa-camd/easey-common/check-catalog';
 import { ConnectionModule } from '@us-epa-camd/easey-common/connection';
 import { DbLookupValidator } from '@us-epa-camd/easey-common/validators';
+import { MaintenanceMiddleware } from '@us-epa-camd/easey-common/middleware/maintenance.middleware';
 
 import routes from './routes';
 import appConfig from './config/app.config';
@@ -85,4 +86,8 @@ import { WhatHasDataModule } from './what-has-data/what-has-data.module';
   ],
   providers: [DbLookupValidator],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(MaintenanceMiddleware).forRoutes({ path: '*', method: RequestMethod.ALL })
+  }
+}
