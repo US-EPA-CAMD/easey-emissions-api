de5c9446c1d43f49d2a1770ef2f2dfd181d7d6d6
Yonatan Amezene <yamezene@decisionpointcorp.com>
2024-12-03 08:20:14 -0500

    Removed the whatHasData Endpoint from Swagger Documentation

--- src/what-has-data/what-has-data.controller.ts
+++ src/what-has-data/what-has-data.controller.ts
@@ -1,5 +1,5 @@
 import { Get, Controller, Query } from '@nestjs/common';
-import { ApiSecurity } from '@nestjs/swagger';
+import { ApiExcludeEndpoint, ApiSecurity } from '@nestjs/swagger';
 
 import { WhatHasDataService } from './what-has-data.service';
 import { WhatHasDataParamsDTO } from '../dto/what-has-data.params.dto';
@@ -10,6 +10,7 @@ export class WhatHasDataController {
   constructor(private service: WhatHasDataService) {}
 
   @Get()
+  @ApiExcludeEndpoint() // Excluding this endpoint from the Swagger documentation because it takes a long time to run during a security scan
   whatHasData(@Query() dto: WhatHasDataParamsDTO): Promise<any> {
     return this.service.whatHasData(dto.dataType, dto.workspace);
   }
